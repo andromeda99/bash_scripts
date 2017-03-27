136a115f2ef4e53284a2f2e655ed6d0da7652625
Aamir Shaikh <aamir.shaikh@networkedinsights.com>
2017-03-27 13:54:50 -0400

    made some changes

--- calc_var_size.sh
+++ calc_var_size.sh
@@ -2,16 +2,35 @@
 echo -e  "\e[1;38mPlease enter any Character/Integer/Alphanumeric Value:- \e[0m" 
 read temp
 echo -e "\e[1;38m\t\t********* Value entered is "\"$temp"\" *********\e[0m"
-regex="^([A-Za-z]+)$"
+regex="^([A-Z\s a-z\s]+)$"
 regex1="^([0-9]+)$"
 regex2="^([A-Za-z0-9]+)$"
 
 if [[ "$temp" =~ $regex ]] 
 then
-	k=`echo -n "$temp" | wc -c`
-	echo -e "\e[1;38m"\"$temp"\" is a $k character word/string. Each character holds 4 byte of memory.\e[0m"
-	k=`echo $k \* 4 | bc`
-	echo -e "\e[1;93m\t\t====== Size of "\"$temp"\" is $k bytes ======\e[0m"
+	if [[ "$temp" =~ " " ]]
+	then
+	echo "String has a whitespace"
+		k=`echo -n "$temp" | wc -c`
+		char=$(grep -o "[A-Za-z]" <<<"$temp")
+		m=`echo $char  | wc -c`
+		m=$((m / 2))
+		echo -e "\e[1;38m"\"$temp"\" is a $k character word/string with a whitespace & $m without a whitespace.\e[0m"
+		echo -e "\e[1;38mEach character holds 4 byte of memory & 1 byte of a whitespace/tab character.\e[0m"
+		m=`echo $m \* 4 | bc`
+		m=`echo $m  \+ 1 | bc`
+	        echo -e "\e[1;93m\t\t====== Size of "\"$temp"\" is $m bytes ======\e[0m"
+	else
+		k=0
+		k=`echo -n "$temp" | wc -c`
+		echo "String does not have a whitespace"
+		echo -e "\e[1;38m"\"$temp"\" is a $k character word/string.\e[0m"
+		char=$(grep -o "[A-Za-z]" <<<"$temp")
+		k=`echo "$char" | wc -c`
+		k=`echo $k  \* 2 | bc`
+		echo -e "\e[1;93m\t\t====== Size of "\"$temp"\" is $k bytes ======\e[0m"
+		
+	fi
 elif [[ "$temp" =~ $regex1 ]]
 then
 	k=`echo -n "$temp" | wc -c`
@@ -31,7 +50,8 @@ m=0
 	char=$(grep -o "[A-Za-z]" <<<"$temp")
         m=`echo $char | wc -m`
         m=`echo $m \* 2 | bc`
-        echo -e "\e[1;38mSize of digits "\"$digit"\" is \e[1;93m$k \e[0m \e[1;38mbytes & Size of characters "\"$char"\" is \e[1;93m$m \e[0m \e[1;38mbytes\e[0m"
+        #echo -e "\e[1;38mSize of digits "\"$digit"\" is \e[1;93m$k \e[0m \e[1;38mbytes & Size of characters "\"$char"\" is \e[1;93m$m \e[0m \e[1;38mbytes\e[0m"
+	echo -e "\e[1;38mSize of characters "\"$char"\" is \e[1;93m$m \e[0m \e[1;38mbytes & Size of digits "\"$digit"\" is \e[1;93m$k \e[0m \e[1;38mbytes\e[0m"
 	k=`echo $k + $m | bc`
 	echo -e "\e[1;93\tm===== Total size of string "\"$temp"\" is $k bytes =====\e[0m"
 	

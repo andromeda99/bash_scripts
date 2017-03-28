4bd9fa7722c850a8a5034c87413531640a20dc1f
Aamir Shaikh <aamir.shaikh@networkedinsights.com>
2017-03-28 10:49:48 -0400

    modified freespace_mod.sh

--- freespace_mod.sh
+++ freespace_mod.sh
@@ -8,37 +8,39 @@ s=0
 sub=0
 for i in /dev/sd{a..z}
 do
-	x=`fdisk -l | grep /dev/sda | sed -n '1p' | awk '{print $4}' | cut -d, -f1`
-	if [ "$x" == "GB" ];then
-        	x=`fdisk -l | grep $i | sed -n '1p' | awk '{print $3}'`
-	   	if [ "$x" = "" ]; then
-        		echo "=============="
-	        	#echo "$i does not exist..Accumalating disks......."
-        		break
-        	else
-	        	echo "Size of $i is $x GB"
-		        y=`echo $x + $y | bc`
-		        d[${sub}]=$i
-	        	((sub +=1))
-		        f[${s}]=$x
-		        ((s +=1))
-        		z=`echo $z + 1 | bc`
-	        fi
+	x=`fdisk -l | grep $i | sed -n '1p' | awk '{print $4}' | cut -d, -f1`
+	if [ "$x" = "" ]; then
+                        echo "=============="
+                        #echo "$i does not exist..Accumalating disks......."
+                        break
 	else
-		echo "OOPS...The size seems to be in MB"
-        	x=`fdisk -l | grep $i | sed -n '1p' | awk '{print $3}'`
-		echo "Its $x MB..Converting it into GB......"
-		sleep 2;
-	        x=`echo "scale=2; $i/1024" | bc`
-	        echo "Converting successful"
-		echo "Size of $i is $x GB"
-		y=`echo $x + $y | bc`
-                d[${sub}]=$i
-                ((sub +=1))
-                f[${s}]=$x
-                ((s +=1))
-                z=`echo $z + 1 | bc`
-        fi
+	    #x=`fdisk -l | grep $i | sed -n '1p' | awk '{print $4}' | cut -d, -f1`
+		if [ "$x" == "GB" ];then
+        		x=`fdisk -l | grep $i | sed -n '1p' | awk '{print $3}'`
+			echo "Size of $i is $x GB"
+                        y=`echo $x + $y | bc`
+                        d[${sub}]=$i
+                        ((sub +=1))
+                        f[${s}]=$x
+                        ((s +=1))
+                        z=`echo $z + 1 | bc`
+		else
+			x=0
+			echo "Size of $i seems to be in MB"
+        		x=`fdisk -l | grep $i | sed -n '1p' | awk '{print $3}'`
+			echo "Its $x MB..Converting it into GB......"
+			sleep 2;
+		        x=`echo "scale=2; $x/1024" | bc`
+	       		echo "Conversion successful"
+			echo "Size of $i is $x GB"
+			y=`echo $x + $y | bc`
+	                d[${sub}]=$i
+        	        ((sub +=1))
+               		f[${s}]=$x
+	                ((s +=1))
+			z=`echo $z \+ 1 | bc`
+		fi
+	fi
 done
 #echo -e "\e[1;38mThere are total $z disks on $HOSTNAME server. Those are ${d[@]} & total size of all disks is $y GB \e[0m"
 echo -e "\e[1;38mTotal Disks:- $z"
